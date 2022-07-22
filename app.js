45cede78b0973e190740960dd17607d3bd9ed4b9
ignacio_perrote <perrote.ignacio@gmail.com>
2022-07-21 21:45:07 -0300

    aprendiendo Js

---
+++ app.js
@@ -0,0 +1,72 @@
+let tabla = Number( prompt('Ingrese el número deseado') )
+
+let tuNumero = tabla
+
+
+ for (let i = 1; i <= 10; i++) {
+
+    console.log(i * tabla)
+
+    if(i=== tuNumero){
+        alert("ESTE ES TU NUMERO DE LA SUERTE " + tabla * 5)
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let sku = prompt("Ingrese el SKU del Producto")
+    
+// if (sku == 120) {
+//     alert("¡LAVARROPAS!")
+// } else if (sku == 240){
+//     alert("¡HORNO!")
+// } else if (sku == 320){
+//     alert("¡HELADERA!")
+// } else{
+//     alert("PRODUCTO NO ENCONTRADO")
+// }
+
+// console.log("El sku del: " + sku)
+
+
+
+// class Producto{
+    
+//     constructor (sku, nombre, stock, precio){
+//     this.sku= sku,
+//     this.nombre= nombre,
+//     this.stock= stock,
+//     this.precio= precio
+//     }
+
+
+
+//     calcularIva = () => {
+//         this.precio= (precio + *1.21)
+
+//     }
+// }
+
+// const Producto1= new Producto(120, "Lavarropas", 5, 65000)
+
