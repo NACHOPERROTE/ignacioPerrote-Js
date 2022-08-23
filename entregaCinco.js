f04801ed855c0ac5bb2c76ee55bae644791c2323
ignacio_perrote <perrote.ignacio@gmail.com>
2022-08-22 21:58:00 -0300

    sistema de monedas

--- entregaCinco.js
+++ entregaCinco.js
@@ -1,6 +1,116 @@
+// Variables
+        // Maquina
+        const num1 = document.querySelector('#num1');
+        const num2 = document.querySelector('#num2');
+        const num3 = document.querySelector('#num3');
+        let num1Value = 0;
+        let num2Value = 0;
+        let num3Value = 0;
+        const boton = document.querySelector('#boton');
+    
+            // Contador de creditos
+        let ingreso = prompt('¿Cuanto dinero juegas?');
+    
+        const creditos = document.querySelector('#creditos');
+        let cuenta = apuesta(ingreso);
+        let activo = true;
+    
+    
+        // Funciones
+        function apuesta(cantidad) {
+            const ingresado = parseFloat(cantidad);
+            creditos.innerHTML = cantidad;
+            return ingresado;
+        }
+    
+        function numerosAleatorios() {
+            num1Value = _.random(0, 9);
+            num2Value = _.random(0, 9);
+            num3Value = _.random(0, 9);
+        }
+    
+        function mostrarNumeros() {
+            num1.innerHTML = num1Value;
+            num2.innerHTML = num2Value;
+            num3.innerHTML = num3Value;
+        }
+    
+        function restarDinero() {
+            cuenta = cuenta - 1;
+            ingreso = cuenta;
+            mostrar();
+        }
+    
+        function sumarDinero() {
+            cuenta = cuenta + 10;
+            ingreso = cuenta;
+            mostrar();
+        }
+    
+        function comprobar() {
+            activo = num1Value === num2Value && num2Value === num3Value;
+        }
+    
+        function mostrar() {
+            creditos.innerHTML = cuenta;
+        }
+    
+        function suerte() {
+            if (activo) {
+                // Se suma el dinero
+                sumarDinero();
+            } else {
+                // Se resta el dinero
+                restarDinero();
+            }
+            if (cuenta === 0) {
+                alert('¡Has perdido!');
+                ingreso = prompt('¿Cuanto dinero juegas?');
+                cuenta = apuesta(ingreso);
+            }
+        }
+    
+        function jugar() {
+            numerosAleatorios();
+            comprobar();
+            mostrarNumeros();
+            suerte();
+            console.log(''.concat(num1Value, num2Value, num3Value,' - ', activo, ' ---> cuenta: ',cuenta));
+        }
+    
+        // Eventos
+        boton.addEventListener('click', jugar);
+    
+        // Inicio
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const btnStar = document.querySelector('#btnStart')     
 const cerrarModal = document.querySelector('#numeros__cerrarModal')   
 const modalContainer = document.querySelector('#message')
+  
+
         
         function getRandomNumber(min,max) {
             return Math.floor(Math.random() * (max - min) + min);
